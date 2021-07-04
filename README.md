0db54c640a589c52ae706b502e103af6e4c27159
Sara-Jayne Terp <sarajterp@gmail.com>
2021-07-04 21:07:33 +0100

    added d3 viz tests

--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 Amitt website code - makes AMITT framework objects accessible to non-technical people.  
 
 Written in Python, Flask, D3.  
+
+Running locally - getting set up
+
+* install postgresql locally https://www.postgresql.org/
+* Use the AMITT repo to generate a database for you https://github.com/cogsec-collaborative/AMITT file HTML_GENERATING_CODE/AMITT_create_website_sql.ipynb will do this for you (run it using jupyter - install that using anaconda https://www.anaconda.com/)
+* Open a terminal window.  git clone this repo. cd into it   
+
+Running locally
+
+* . venv/bin/activate; 
+* export FLASK_APP=amittsite; export FLASK_ENV=development; export DATABASE_URL2="postgresql:///amittsite"
+* flask run
+* go to http://127.0.0.1:5000/
+
+
+Running on Heroku - getting set up
+
+* Create a heroku account. Create an app. 
+* Click on the app, then the resources tab. Add the Postgres add-on.  
+* go to https://dashboard.heroku.com/apps/<your heroku app name>/settings
+* click "reveal config vars"
+* Edit DATABASE_URL: take a copy of it. It will start with "postgres://"
+* Create DATABASE_URL2: copy DATABASE_URL into it, but start it with start with "postgresql://" instead
+
+Running on Heroku - updating Heroku code and database
+
+* pg_dump -Fc -h localhost -U <yourdatabaseusername> amittsite > amittsite.dump  
+* heroku pg:reset -a <your heroku app name>   
+* heroku pg:push amittsite DATABASE_URL -a <your heroku app name> 
+* git add .; git commit -m "heroku stuff"; git push heroku main
--- amittsite/static/textgrid.js
+++ amittsite/static/textgrid.js
@@ -1,24 +1,24 @@
 
-var techniques = [
-  ['TA01', 'TA02', 'TA03', 'TA04', 'TA05', 'TA06', 'TA07', 'TA08', 'TA09', 'TA10', 'TA11', 'TA12'],
-  ['T0001', 'T0005', 'T0007', 'T0010', 'T0016', 'T0019', 'T0029', 'T0039', 'T0047', 'T0057', 'T0058', 'T0062'],
-  ['T0002', 'T0006', 'T0008', 'T0011', 'T0017', 'T0020', 'T0030', 'T0040', 'T0048', 'T0061', 'T0059', 'T0063'],
-  ['T0003', '', 'T0009', 'T0012', 'T0018', 'T0021', 'T0031', 'T0041', 'T0049', '', 'T0060', 'T0064'],
-  ['T0004', '', '', 'T0013', '', 'T0022', 'T0032', 'T0042', 'T0050', '', '', ''],
-  ['', '', '', 'T0014', '', 'T0023', 'T0033', 'T0043', 'T0051', '', '', ''],
-  ['', '', '', 'T0015', '', 'T0024', 'T0034', 'T0044', 'T0052', '', '', ''],
-  ['', '', '', '', '', 'T0025', 'T0035', 'T0045', 'T0053', '', '', ''],
-  ['', '', '', '', '', 'T0026', 'T0036', 'T0046', 'T0054', '', '', ''],
-  ['', '', '', '', '', 'T0027', 'T0037', '', 'T0055', '', '', ''],
-  ['', '', '', '', '', 'T0028', 'T0038', '', 'T0056', '', '', '']
-  ];
+// var techniques = [
+//   ['TA01', 'TA02', 'TA03', 'TA04', 'TA05', 'TA06', 'TA07', 'TA08', 'TA09', 'TA10', 'TA11', 'TA12'],
+//   ['T0001', 'T0005', 'T0007', 'T0010', 'T0016', 'T0019', 'T0029', 'T0039', 'T0047', 'T0057', 'T0058', 'T0062'],
+//   ['T0002', 'T0006', 'T0008', 'T0011', 'T0017', 'T0020', 'T0030', 'T0040', 'T0048', 'T0061', 'T0059', 'T0063'],
+//   ['T0003', '', 'T0009', 'T0012', 'T0018', 'T0021', 'T0031', 'T0041', 'T0049', '', 'T0060', 'T0064'],
+//   ['T0004', '', '', 'T0013', '', 'T0022', 'T0032', 'T0042', 'T0050', '', '', ''],
+//   ['', '', '', 'T0014', '', 'T0023', 'T0033', 'T0043', 'T0051', '', '', ''],
+//   ['', '', '', 'T0015', '', 'T0024', 'T0034', 'T0044', 'T0052', '', '', ''],
+//   ['', '', '', '', '', 'T0025', 'T0035', 'T0045', 'T0053', '', '', ''],
+//   ['', '', '', '', '', 'T0026', 'T0036', 'T0046', 'T0054', '', '', ''],
+//   ['', '', '', '', '', 'T0027', 'T0037', '', 'T0055', '', '', ''],
+//   ['', '', '', '', '', 'T0028', 'X0038', '', 'T0056', '', '', '']
+//   ];
 
 
-var grid_div = "#redgrid";
-fillGrid(techniques, grid_div);
+// var grid_div = "#redgrid";
+// fillGrid(techniques, grid_div);
 
 
-function fillGrid(techniques, gridname) {
+function fillGrid(techniques, gridname, topcolor) {
 
 	var numrows = techniques.length;
 	var numcols = techniques[0].length;
@@ -34,6 +34,7 @@ function fillGrid(techniques, gridname) {
 	for (var row = 0; row < numrows; row++) {
 		for (var col = 0; col < numcols; col++) {
 			var text = techniques[row][col];
+			var color = (row == 0) ? topcolor : '#D0D3D4';
       if (text != '') {
 				data.push({
 					x: xpos,
@@ -41,6 +42,7 @@ function fillGrid(techniques, gridname) {
 					width: boxwidth,
 					height: boxheight,
 					text: text,
+					startcolor: color,
 					click: boxclick
 				})
 		  }
@@ -73,11 +75,11 @@ function fillGrid(techniques, gridname) {
 		.attr("text", function(d) { return d.text; })
 		.attr("width", function(d) { return d.width; })
 		.attr("height", function(d) { return d.height; })
-		.style("fill", "#D0D3D4")
+		.style("fill", function(d) { return d.startcolor; })
 		.style("stroke", "#222")
 		.on('click', function(d) {
 	       d.click ++;
-	       if ((d.click)%2 == 0 ) { d3.select(this).style("fill","#D0D3D4"); }
+	       if ((d.click)%2 == 0 ) { d3.select(this).style("fill",function(d) { return d.startcolor;}); }
 		   if ((d.click)%2 == 1 ) { d3.select(this).style("fill","#50C878"); }
 	  });
 
